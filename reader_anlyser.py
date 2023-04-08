7bb52a2a258a45c5887a015a9cb7204797d32b82
Arjun <arjunsharma0710@gmail.com>
2023-04-07 21:48:51 -0400

    Error handling for opening files. Feature - accommodate password protected files.

--- reader_anlyser.py
+++ reader_anlyser.py
@@ -1,13 +1,33 @@
+import os
 import PyPDF2
 
 file = input("Enter the full path of the pdf file: ")
 
-reader = PyPDF2.PdfReader(file)
-"""
-To do:
-error handling if the pdf file does not open.
+if not os.path.isfile(file):
+    print("Error: File does not exist. Please check the path again.")
 
-"""
+else:
+    try:
+        reader = PyPDF2.PdfReader(file)
+        if reader.is_encrypted:
+            print("\nFile is encrypted.\n")
+            response = 'y'
+            while response=='y':
+                password = input("Enter the password to decrypt the pdf file: ")
+                if reader.decrypt(password):
+                    break
+                else:
+                    print("Wrong password.")
+                    response = input("Try again? y/n: ")
+
+            if reader.decrypt(password):
+                print("Successfully decrypted. File opened successfully.\n")
+        else:
+            print("File opened successfully.")
+    except PyPDF2.utils.PdfReadError:
+        print("Error: unable to read PDF file")
+    except Exception as e:
+        print(f"Error: {e}")
 
 for i in range(len(reader.pages)):
     page = reader.pages[i]
